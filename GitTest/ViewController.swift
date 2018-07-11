f5eca5aefc703f9fe82fccaa71ba205ffa32d616
“kokirk” <ko.kirk@gmail.com>
2018-07-11 22:24:01 +0800

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Kirk Ko on 2018/7/11.
+//  Copyright © 2018年 Kirk Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
